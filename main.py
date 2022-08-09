e38edbbc8dc572f87bdb3d7a166fe981ece7daa7
Gamerrio <Gamerrio84@gmail.com>
2022-08-09 15:41:26 +0530

    Client can foward input to mcc now

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk ,Gdk,GLib
+
+import os
+import sys 
+from subprocess import Popen, PIPE, STDOUT
+import threading
 
 #Handles the events like button presses
 class Handler:
@@ -14,6 +19,7 @@ class Handler:
         notebook = builder.get_object("notebook")
         #opens the new tab with vb and adds it to the notebook
         page = chat_area()
+        print(notebook)
         notebook.append_page(page, Gtk.Label(ip.get_text()))
         win.show_all()
 
@@ -22,7 +28,10 @@ class Handler:
 def chat_area():
             vb = Gtk.VBox()
             hb = Gtk.HBox()
-            tv = vb.pack_start(Gtk.TextView(),expand=True,fill=True,padding=0)
+            output = Gtk.TextView()
+            sw = Gtk.ScrolledWindow()
+            sw.add_with_viewport(output)
+            tv = vb.pack_start(sw,expand=True,fill=True,padding=0)
             btn = Gtk.Button()
 
             snd_image = Gtk.Image()
@@ -33,8 +42,34 @@ def chat_area():
             hb.pack_start(ety, expand = True, fill = True, padding = 0)
             hb.pack_start(btn, expand = False, fill = True, padding = 0)
             vb.pack_start(hb, expand = False, fill = True, padding = 6)
+
+            #starts a new thread for taking the chat from a console window so it does not break GUI loop
+            threading.Thread(target=chat, args=(output,btn,ety,)).start()
             return vb
 
+
+def chat(output,btn,ety):
+    p = Popen(['mono', 'MinecraftClient.exe',name.get_text(),password.get_text(),ip.get_text(),'BasicIO'],
+          stdout=PIPE, stderr=STDOUT,stdin=PIPE)
+    def send_input(dummy,p,ety,*args):
+        text=ety.get_text()+'\n'
+        p.stdin.write(text.encode())
+        p.stdin.flush()
+        ety.set_text('')
+    
+    btn.connect("clicked", send_input,p,ety)
+    with p.stdout:
+        for line in iter(p.stdout.readline, b''):
+            Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE,append_text, output,line.decode("utf-8")),
+
+    p.wait()
+    return p
+
+#appends the text in Textview
+def append_text(output,text):
+    text_buffer = output.get_buffer()
+    end_iter = text_buffer.get_end_iter()
+    text_buffer.insert(end_iter, text)
 
 #Getting the UI Components
 builder = Gtk.Builder()
